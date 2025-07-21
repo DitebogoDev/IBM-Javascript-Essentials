0271b78c833736ecb05860cba99b65412aa1e0e7
DitebogoDev <156467536+DitebogoDev@users.noreply.github.com>
2025-07-21 22:14:25 +0200

    updated functions

--- CalculateArea/calculate_Area.js
+++ CalculateArea/calculate_Area.js
@@ -9,3 +9,14 @@ function calculateArea() {
 
   document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+function calculateAmount() {
+ let grocery1 = parseFloat(document.getElementById("grocery1").value) || 0;
+ let grocery2 = parseFloat(document.getElementById("grocery2").value) || 0;
+ let grocery3 = parseFloat(document.getElementById("grocery3").value) || 0;
+
+ let totalAmount = grocery1 + grocery2 + grocery3;
+
+
+  document.getElementById('result2').innerText = `The total amount is: R${totalAmount}`;
+}
